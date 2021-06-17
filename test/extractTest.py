5f4b64a5bf215cc83ab4bb55fe7438995eac2881
Siwei Yang <syan553@aucklanduni.ac.nz>
2021-06-18 10:45:11 +1200

    First try, f0 extraction
    17/Jun/21
    
    Co-authored-by: skyn310198 <skyn310198@gmail.com>

---
+++ test/extractTest.py
@@ -0,0 +1,20 @@
+from librosa import load, display, yin
+import matplotlib.pyplot as plt
+
+def extractTest():
+    testAudioInput = '../inputFile/P16.wav'
+    x , sr = load(testAudioInput)
+    print(type(x), type(sr))
+    #plt.figure(figsize=(14, 5))
+    #display.waveplot(x[:100], sr=sr)
+    #plt.show()
+    #Observe amplitude changes can infer where speech occors
+    f0Result = yin(x, 65, 3000, sr=sr*10, frame_length=1764)
+    print(f0Result[:50])
+
+    
+    print("finished")
+    return 0
+
+if __name__ == "__main__":
+    extractTest()
