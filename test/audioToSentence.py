6d0620d2597cfab77870e7224fb41f7ef0cb1f76
Siwei Yang <syan553@aucklanduni.ac.nz>
2021-08-24 03:52:57 +1200

    add py version of  a2s, fixed walk method

---
+++ test/audioToSentence.py
@@ -0,0 +1,64 @@
+import os
+import pandas as pd
+from collections import namedtuple
+
+import base64
+bStart = \
+'''
+print('Process Start')
+'''
+bStart = base64.b64encode(bytes(bStart, 'utf-8'))
+
+bEnd = \
+'''
+print('Tasks are completed')
+'''
+bEnd = base64.b64encode(bytes(bEnd, 'utf-8'))
+
+inputPath = '../inputFile/modelInput/'
+outputPath = '../inputFile/modelInput/sentences/'
+eval(compile(base64.b64decode(bStart), "<string>", 'exec'))
+sentencesTuple = namedtuple('Sentences', ['Time', 'Valence', 'Arousal', 'RMS', 'F0Log10', 'MFCC1' , 'MFCC2' , 'MFCC3', 'MFCC4', 'MFCC5'])
+lengthDistributionDf = pd.DataFrame()
+for dir, _, filenames in os.walk(inputPath):
+    if (dir.find('sentences') != -1):
+        continue
+    filenames.remove('allFileCombineU.csv'); filenames.remove('allFileCombineP.csv');
+    for file in filenames:
+        count = 0
+        currentDf = pd.DataFrame()
+        fileName = file[:file.rfind('.')]
+        fileDf = pd.read_csv(os.path.join(dir, file))
+        # min_length of voice in ms
+        min_length = 1000
+        if fileName.find('reco'):
+            min_length /= 40
+        elif fileName.find('sema'):
+            min_length /= 20
+        for row in fileDf.itertuples(index=False):
+            if (row[-1] == 'V'):
+                # use * to passes in each element of the row sequence as a separate argument
+                currentDf = pd.concat([currentDf, pd.DataFrame([sentencesTuple(*row[:-1])])], ignore_index=True)
+            elif (row[-1] == 'S' and currentDf.shape[0] > min_length):
+                # e.g sema0126tu001
+                currentName = fileName+str(count).zfill(3);
+                # increase file name
+                count += 1
+                # write to dict
+                currentDf.to_csv(outputPath+currentName+'.csv', index=False)
+                lengthDistributionDf = pd.concat([lengthDistributionDf, pd.DataFrame({"Name": [currentName], "Length":[currentDf.shape[0]]})])
+                # empty df
+                currentDf = pd.DataFrame()
+            else:
+                pass
+                
+        # if the last element is V, save
+        if (currentDf.shape[0] > min_length):
+            currentName = fileName+str(count).zfill(3);
+            currentDf.to_csv(outputPath+currentName+'.csv', index=False)
+            lengthDistributionDf = pd.concat([lengthDistributionDf, pd.DataFrame({"Name": [currentName], "Length":[currentDf.shape[0]]})])
+        else:
+            pass
+
+lengthDistributionDf.to_csv(outputPath+"lengthDistribution"+'.csv', index=False)
+eval(compile(base64.b64decode(bEnd), "<string>", 'exec'))
